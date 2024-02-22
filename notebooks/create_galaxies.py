2931c6a64911d7a69a33186a5b3c76d51f81a398
Laura Cabayol <lauracabayol@gmail.com>
2024-02-22 09:13:30 +0100

    add cuts before starting to create the catalogue

--- notebooks/create_galaxies.py
+++ notebooks/create_galaxies.py
@@ -28,7 +28,7 @@ from Flagship4ML.create_sims import create_simulated_images
 # %%
 from dask.distributed import Client
 
-client = Client("tls://192.168.101.78:33862")
+client = Client("tls://192.168.101.78:43830")
 client
 
 # %%
@@ -37,32 +37,39 @@ catalogue = pd.read_parquet('/data/astro/scratch2/lcabayol/NFphotoz/data/15527.p
                     )
 
 # %%
+catalogue['imag'] = -2.5 * np.log10(catalogue.cfht_megacam_i_1_el_model3_odonnell_ext_error) - 48.6 
+catalogue = catalogue[catalogue.imag < 24]
+
+# %%
 t0=time.time()
 ImageSimulator = create_simulated_images(catalogue,
-                                         Ngals=1_000,
+                                         Ngals=10_000,
                                          bands=['CFHT_U', 'CFHT_G', 'CFHT_R', 'CFHT_I', 'CFHT_Z'],
                                          add_poisson=True,
+                                         add_psf=True,
                                          add_constant_background=True,
                                          use_dask=True,
-                                         output_dir='/data/astro/scratch2/lcabayol/NFphotoz/data/CHFT_sims_bkg_test/',
+                                         num_exposures=3,
+                                         output_dir='/data/astro/scratch2/lcabayol/NFphotoz/data/CHFT_sims_bkg_psf_mexp_v2/',
                                         )
 
 print(time.time()-t0)
 
 # %%
 band = 'CFHT_U'
-ii = 100
+ii = 2
+exp=1
 
-t = np.load(f'/data/astro/scratch2/lcabayol/NFphotoz/data/CHFT_sims_bkg/data_{ii}/cutout_{band}.npy')
-m= np.load(f'/data/astro/scratch2/lcabayol/NFphotoz/data/CHFT_sims_bkg/data_{ii}/metadata_{band}.npy')
+t = np.load(f'/data/astro/scratch2/lcabayol/NFphotoz/data/CHFT_sims_bkg_psf_mexp_test/data_{ii}/cutout_{band}_exp{exp}.npy')
+m= np.load(f'/data/astro/scratch2/lcabayol/NFphotoz/data/CHFT_sims_bkg_psf_mexp_test/data_{ii}/metadata_{band}_exp{exp}.npy')
+
+# %%
+a=1
 
 # %%
 import matplotlib.pyplot as plt
 plt.imshow(t)
 plt.colorbar()
-
-# %%
-t.sum()
 
 # %%
 m
