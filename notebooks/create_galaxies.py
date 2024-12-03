cdcee525a7ac69c703a999f1216c7af396a7e0b3
Laura Cabayol Garcia <lauracabayol@Lauras-MacBook-Pro.local>
2024-12-03 16:44:31 +0100

    update notebook

--- notebooks/create_galaxies.py
+++ notebooks/create_galaxies.py
@@ -27,6 +27,8 @@ import pandas as pd
 from pathlib import Path
 
 from Flagship4ML.f4ml.sims_generator import CreateSimulatedImages
+from Flagship4ML.f4ml.constants import DATA_DIR
+
 
 # %% [markdown]
 # ## 1. DEFINE THE CATALOGUE
@@ -36,35 +38,38 @@ from Flagship4ML.f4ml.sims_generator import CreateSimulatedImages
 #
 
 # %%
-catalogue = Path("/data/astro/scratch/lcabayol/NFphotoz/data/16511_PAU.parquet")
+catalogue = DATA_DIR / "16511_PAU_subsample_100.parquet"
 
 # %% [markdown]
 # ### Define the bands you want to use.
 
 # %%
 # bands = ['CFHT_U', 'CFHT_G', 'CFHT_R', 'CFHT_I', 'CFHT_Z']
-# bands = [f'pau_nb{x}' for x in np.arange(455,855,10)]
-bands_el = [f"pau_nb{x}_el" for x in np.arange(455, 855, 10)]
+bands = [f'pau_nb{x}' for x in np.arange(455,855,10)]
+#bands_el = [f"pau_nb{x}_el" for x in np.arange(455, 855, 10)]
 
 # %% [markdown]
 # # 2. CREATE THE SIMULATED IMAGES
 
 # %%
-ImageSimulator = create_simulated_images(
+ImageSimulator = CreateSimulatedImages(
     catalogue=catalogue,
     bands=bands,
     crop_size=60,
     resolution=10,
-    Ngals=1_000,
+    Ngals=1,
     add_poisson=True,
     add_psf=True,
     add_constant_background=True,
     use_dask=False,
     calibrate_flux=True,
     num_exposures=3,
-    output_dir="/data/astro/scratch/lcabayol/NFphotoz/data/PAUS_sims_test_v3/",
+    output_dir=DATA_DIR /"tests",
 )
 
+
+# %%
+ImageSimulator.generate_simulated_catalogue()
 
 # %% [markdown]
 # ## ONE CAN ALSO CREATE GALAXIES USING THE CREATE_SIMS.PY SCRIPT.
